f7c3c55ecbd0f08e5a92ffe73cc04c28db0a4d4c
Lex <pineapplepear55@gmail.com>
2023-06-22 09:44:51 +0300

    feat: add sorting do dir ls

--- src/components/dirMgmt.js
+++ src/components/dirMgmt.js
@@ -35,23 +35,43 @@ class DirMgmt {
 
   parseDirectoryList(files) {
     let returnList = [];
-    for (let i = 0; i < files.length; i++) {
-      const fileType = files[i].isDirectory() ? 'directory' : 'file';
-      if (files[i].name.length > 25) {
-        returnList = returnList.concat(this.splitLine(files[i].name, fileType));
+    for (const item of files) {
+      const fileType = item.isDirectory() ? 'directory' : 'file';
+      console.log(fileType);
+      returnList.push({ name: item.name , type: fileType })
+    }
+    returnList = this.sortList(returnList);
+    returnList = this.splitLines(returnList);
+    return returnList;
+  }
+
+  sortList(list) {
+    const typeSorted = list.sort((a, b) => a.type > b.type ? 1 : -1 );
+    const dirSort = list.sort((a, b) => {
+      if (a.type == b.type) {
+        return a.name.localeCompare(b.name);
+      }
+    })
+    return dirSort;
+  }
+
+  splitLines(list) {
+    let returnList = [];
+    for (const item of list) {
+      if (item.name.length > 25) {
+        returnList = returnList.concat(this.splitLine(item));
       } else {
-        returnList.push({ name: files[i].name, type: fileType });
+        returnList.push(item);
       }
     }
     return returnList;
   }
 
-  splitLine(line, type) {
-    const fin = Math.floor(line.length/25);
+  splitLine(item) {
     let retLine = [];
-    for (let i = 0; i < line.length; i += 25) {
-      if (i < 25) retLine.push({ name: line.slice(i, i+25), type });
-      else retLine.push({ name: line.slice(i, i+25) });
+    for (let i = 0; i < item.name.length; i += 25) {
+      if (i < 25) retLine.push({ name: item.name.slice(i, i+25), type: item.type });
+      else retLine.push({ name: item.name.slice(i, i+25) });
     }
     return retLine;
   }
@@ -62,7 +82,7 @@ class DirMgmt {
       this.parseDirectoryList(files)
       console.table(this.parseDirectoryList(files));
     } catch (error) {
-      CustomOutput.logError(error.message);
+      CustomOutput.logError(error);
     }
   }
 }
